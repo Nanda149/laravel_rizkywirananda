be23e213288d9b0200de57ff476bae140145c80b
Nanda149 <rizkywirananda69@gmail.com>
2024-05-02 04:20:06 +0700

    Controller

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,6 @@
 <?php
 
+use App\Http\Controllers\produkController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('about', function () {
-    return view('about');
-});
-
-Route::get('mahasiswa', function () {
-    return view('mahasiswa');
-});
-
-Route::get('profile', function () {
-    $nama = 'Nanda';
-    // return view('profile', compact('nama'));
-    return view('profile')->with('nama', $nama);
-});
+Route::resource('produk', produkController::class);
